9729a4771b4006fe179f98835182c9a0b7de3907
Kaustubh Ajgaonkar <kaustubhajgaonkar43@gmail.com>
2023-03-29 21:33:29 +0530

    Day-2🚧

--- scrape.py
+++ scrape.py
@@ -2,5 +2,8 @@ import requests
 from bs4 import BeautifulSoup
 
 res = requests.get('https://news.ycombinator.com/')  # Analogous to a web browser without a window
-soup = BeautifulSoup(res.text, 'html.parser')
-print(soup.find(id='score_35325693'))
+soup = BeautifulSoup(res.text, 'html.parser')  # Soup object
+links = soup.select('.titleline')
+votes = soup.select('.score')
+print(votes[0].get('id'))
+
